240d9058dea7bd6dd0efe39491d69ab7233e9382
Brennan Neilson <bvneilson@gmail.com>
2020-01-23 21:29:30 -0700

    updating port

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const postRoutes = require('./api/postRoutes');
 
 const server = express();
-const port = 4001;
+const port = process.env.PORT || 5008;
 
 server.use(express.json());
 
 server.use('/api/posts', postRoutes);
 
 server.listen(port, () => console.log("Listening for changes on port " + port));
+
+module.exports = server;
